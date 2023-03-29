3c7fe9d3085e589b40ec825287c4a41f3fb82742
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-03-29 19:36:51 -0300

    Initial commit

---
+++ backend/editais-upe/src/main/java/com/upe/editais/editaisupe/EditaisUpeApplication.java
@@ -0,0 +1,13 @@
+package com.upe.editais.editaisupe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditaisUpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EditaisUpeApplication.class, args);
+	}
+
+}
---
+++ backend/editais-upe/src/test/java/com/upe/editais/editaisupe/EditaisUpeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upe.editais.editaisupe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EditaisUpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

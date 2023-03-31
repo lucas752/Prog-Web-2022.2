da51a024ff5304f71ba032329d0ca087bb97f2f6
rikcav <henriquearau000@gmail.com>
2023-03-31 18:07:35 -0300

    feat: repositório do usuário geral

---
+++ backend/editais-upe/src/main/java/com/upe/editais/editaisupe/repositories/IGeneralUserRepository.java
@@ -0,0 +1,9 @@
+package com.upe.editais.editaisupe.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.upe.editais.editaisupe.models.GeneralUser;
+
+public interface IGeneralUserRepository extends CrudRepository<GeneralUser, Long> {
+
+}

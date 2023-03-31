fbddfe59b0428dda3b8a80d15f8b9d72d9b800ce
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-03-31 19:03:14 -0300

    add: adicionando o botão do login

--- frontend/src/components/template/LoginArea/LoginArea.jsx
+++ frontend/src/components/template/LoginArea/LoginArea.jsx
@@ -1,19 +1,25 @@
+import { Button } from "../../atomic/Button/Button";
 import { Input } from "../../atomic/Input/Input";
 
 export function LoginArea(){
     return(
         <div>
-            
+
                 <div>logo</div>
 
                 <div>
-                    <Input type="text" place="Email:" />
+                    <p>Email</p>
+                    <Input type="text" place="Digite seu email:" />
                 </div>
                 <div>
-                    <Input type="password" place="Senha:" />
+                    <p>Senha</p>
+                    <Input type="password" place="Digite seu senha:" />
                 </div>
                 
-                <div>botao</div>
+                <div>
+                    <Button name="Login" style="text-[#1C3C78] border rounded-[12px] p-[10px] w-[140px] h-[42px] "/>
+                </div>
+
                 <div>cadastro</div>
         </div>
     )

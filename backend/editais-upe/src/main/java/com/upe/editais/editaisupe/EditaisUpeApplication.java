9149784961843dcff237517a07c41aa818ddc5f1
rikcav <henriquearau000@gmail.com>
2023-03-31 18:09:58 -0300

    test: teste das operações CRUD da api

--- backend/editais-upe/src/main/java/com/upe/editais/editaisupe/EditaisUpeApplication.java
+++ backend/editais-upe/src/main/java/com/upe/editais/editaisupe/EditaisUpeApplication.java
@@ -1,13 +1,36 @@
 package com.upe.editais.editaisupe;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.upe.editais.editaisupe.models.GeneralUser;
+import com.upe.editais.editaisupe.repositories.IGeneralUserRepository;
 
 @SpringBootApplication
 public class EditaisUpeApplication {
 
+	@Autowired
+	IGeneralUserRepository uRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(EditaisUpeApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			GeneralUser user1 = new GeneralUser(
+					"Henrique",
+					"12345678",
+					"henrique@email.com",
+					"minhasenha123",
+					"Coordenador");
+
+			uRepository.save(user1);
+		};
 	}
 
 }

f5ecab8382bb25794cc07cd40ca90d0d1e721633
Nome <miranhaomi@gmail.com>
2023-04-01 14:45:18 -0300

    Atualizando o AuthController.java
    O AuthController por enquanto tem apenas a parte de cadastrar um novo usuário, ele usa o repositório IGeneralRepository e usa o PasswordEnconder do spring security para codificar a senha do usuário antes de enviar ao banco de dados. O controller usa o path /auth e para cadastro usa o /register. O controller ainda tem uma condição para saber se o usuário é um coordenador ou não.

--- backend/editais-upe/src/main/java/com/upe/editais/editaisupe/controllers/AuthController.java
+++ backend/editais-upe/src/main/java/com/upe/editais/editaisupe/controllers/AuthController.java
@@ -1,3 +1,42 @@
 package com.upe.editais.editaisupe.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.upe.editais.editaisupe.models.GeneralUser;
+import com.upe.editais.editaisupe.repositories.IGeneralUserRepository;
+
+@RestController
+@RequestMapping(path = "/auth")
 public class AuthController {
+
+    @Autowired
+    private IGeneralUserRepository uRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public GeneralUser registerUser(@RequestBody GeneralUser newUser) {
+
+        // Faz hash da senha do usuário antes de salvar no banco de dados
+        String encodedPassword = passwordEncoder.encode(newUser.getPassword());
+        newUser.setPassword(encodedPassword);
+
+        // Verifica se o usuário é um coordenador e define suas permissões de acordo
+        if(newUser.getPermission().equals("COORDENADOR")) {
+            newUser.setPermission("COORDENADOR");
+        } else {
+            newUser.setPermission("USUÁRIO");
+        }
+
+        // Salva o novo usuário no banco de dados
+        return uRepository.save(newUser);
+    }
+    
+    
 }

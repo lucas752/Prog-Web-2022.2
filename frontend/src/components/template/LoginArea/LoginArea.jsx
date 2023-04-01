5760873bd82864221601414171380004e1cdb154
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-04-01 15:09:30 -0300

    feat: validando campo de email

--- frontend/src/components/template/LoginArea/LoginArea.jsx
+++ frontend/src/components/template/LoginArea/LoginArea.jsx
@@ -3,11 +3,28 @@ import { Input } from "../../atomic/Input/Input";
 import { Logo } from "../../atomic/Logo/Logo";
 import logoWhite from "../../../assets/image 5 1.png"
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export function LoginArea(){
+
+    const [email, setEmail] = useState("");
+    const [emailValidad, setEmailValidad] = useState()
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value)
+        
+    }
+
+    function emailTest(){ // falta instanciar, mas está funcionando 
+        const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        setEmailValidad(regex.test(email))
+    }
+
+    
+
     return(
         <div className="flex justify-center items-center">
-                <div className="flex flex-col items-center justify-center bg-[#1C3C78] max-w-max  p-[40px]  rounded-[12px] text-[#fff] shadow-2xl shadow-indigo-500/40">
+                <div className="flex flex-col items-center justify-center bg-[#1C3C78] max-w-max p-[40px]  rounded-[12px] text-[#fff] shadow-2xl shadow-indigo-500/40">
 
                 
                 <div>
@@ -15,20 +32,23 @@ export function LoginArea(){
                 </div>
 
                 <div className="mt-[50px] mb-[20px]">
-                    <p className="my-[4px]">Email</p>
-                    <Input type="text" place="Digite seu email:" />
+                    <p className="my-[4px] text-sm">Email</p>
+                    <Input type="email" place="Digite seu email:" func={handleEmailChange} />
+                    {!emailValidad &&  <p className="text-[#EC2026] text-[10px] m-[5px]">Adicione um email válido</p>} 
+                    
                 </div>
                 <div className="mb-[50px]">
-                    <p className="my-[4px]">Senha</p>
+                    <p className="my-[4px] text-sm ">Senha</p>
                     <Input type="password" place="Digite seu senha:" />
+                    
                 </div>
                 
                 <div className="">
-                    <Button name="Login" style="text-[#1C3C78] text-[16px] font-bold border-none rounded-[12px]  w-[100px] h-[30px] shadow-2xl shadow-indigo-500/40  bg-[#fff] duration-500 hover:bg-[#EC2026] hover:text-[#fff] hover:shadow-inner"/>
+                    <Button name="Login"  style="text-[#1C3C78] text-[16px] font-bold border-none rounded-[12px]  w-[100px] h-[30px] shadow-2xl shadow-indigo-500/40  bg-[#fff] duration-500 hover:bg-[#EC2026] hover:text-[#fff] hover:shadow-inner"/>
                 </div>
 
-                <div>
-                    <p>Não possui conta? <Link to="/singup"> Cadastrar</Link> </p>
+                <div className="mt-[20px]">
+                    <p className="text-sm ">Não possui conta? <Link to="/singup" className="text-[#EC2026]"> Cadastrar</Link> </p>
                 </div>
 
                 </div>

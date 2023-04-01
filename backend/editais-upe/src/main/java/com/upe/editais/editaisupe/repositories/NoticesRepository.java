939a846dea0aa49bef94cd7fbb814a2e71be85e0
Nome <miranhaomi@gmail.com>
2023-04-01 14:46:16 -0300

    Deletando o NoticesRepository.java

--- backend/editais-upe/src/main/java/com/upe/editais/editaisupe/repositories/NoticesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.upe.editais.editaisupe.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.upe.editais.editaisupe.models.Notices;
-
-public interface NoticesRepository extends CrudRepository<Notices, Long>{
-
-}

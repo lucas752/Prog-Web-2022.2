ec98cb2f6774a25523225c78667caf7e0d117560
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-04-01 12:33:14 -0300

    add: criando roda para cadastro

--- frontend/src/components/template/LoginArea/LoginArea.jsx
+++ frontend/src/components/template/LoginArea/LoginArea.jsx
@@ -1,20 +1,25 @@
 import { Button } from "../../atomic/Button/Button";
 import { Input } from "../../atomic/Input/Input";
+import { Logo } from "../../atomic/Logo/Logo";
+import logoWhite from "../../../assets/image 5 1.png"
+import { Link } from "react-router-dom";
 
 export function LoginArea(){
     return(
         <div className="flex justify-center items-center">
-                <div className="flex flex-col items-center justify-center bg-[#1C3C78] max-w-max p-[40px] shadow-2xl rounded-[12px] text-[#fff]">
+                <div className="flex flex-col items-center justify-center bg-[#1C3C78] max-w-max  p-[40px]  rounded-[12px] text-[#fff] shadow-2xl shadow-indigo-500/40">
 
                 
-                <div>logo</div>
+                <div>
+                    <Logo img={logoWhite} />
+                </div>
 
                 <div className="mt-[50px] mb-[20px]">
-                    <p>Email</p>
+                    <p className="my-[4px]">Email</p>
                     <Input type="text" place="Digite seu email:" />
                 </div>
                 <div className="mb-[50px]">
-                    <p>Senha</p>
+                    <p className="my-[4px]">Senha</p>
                     <Input type="password" place="Digite seu senha:" />
                 </div>
                 
@@ -22,7 +27,9 @@ export function LoginArea(){
                     <Button name="Login" style="text-[#1C3C78] text-[16px] font-bold border-none rounded-[12px]  w-[100px] h-[30px] shadow-2xl shadow-indigo-500/40  bg-[#fff] duration-500 hover:bg-[#EC2026] hover:text-[#fff] hover:shadow-inner"/>
                 </div>
 
-                <div>cadastro</div>
+                <div>
+                    <p>Não possui conta? <Link to="/singup"> Cadastrar</Link> </p>
+                </div>
 
                 </div>
         </div>

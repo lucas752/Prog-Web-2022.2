b472881ce03e35484b01289dce0c40280455d6bb
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-04-02 13:11:28 -0300

    fix: corrigindo problema de tranbordmento d tela

--- frontend/src/components/organism/Footer/Footer.jsx
+++ frontend/src/components/organism/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import logoWhite from "../../../assets/image 5 1.png"
 export function Footer(){
     return(
         <div>
-            <div className="bg-[#1C3C78]  w-screen flex flex-row justify-end">
+            <div className="bg-[#1C3C78]  flex flex-row justify-end">
                 <Logo img={logoWhite}/>
             </div>
             <div>

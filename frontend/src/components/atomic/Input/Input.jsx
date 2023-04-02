76ca553bb41496dd2a3b78b3f2937ddc198f9676
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-04-02 12:25:23 -0300

    add: commit de segurança

--- frontend/src/components/atomic/Input/Input.jsx
+++ frontend/src/components/atomic/Input/Input.jsx
@@ -1,7 +1,7 @@
 export function Input({type, place, func}){
     return(
         <div>
-            <input className="rounded-[12px] p-[5px] text-[#000] " type={type} placeholder={place} onChange={func} />
+            <input className="rounded-[12px] p-[5px] text-[#000]" type={type} placeholder={place} onChange={func} />
         </div>
     )
 }

559373dc846d9d02c3819e09028886522a3d6e5a
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-04-02 13:16:02 -0300

    add: finlizndo primeira versao do footer

--- frontend/src/components/organism/Footer/Footer.jsx
+++ frontend/src/components/organism/Footer/Footer.jsx
@@ -4,11 +4,11 @@ import logoWhite from "../../../assets/image 5 1.png"
 export function Footer(){
     return(
         <div>
-            <div className="bg-[#1C3C78]  flex flex-row justify-end">
-                <Logo img={logoWhite}/>
+            <div className="bg-[#1C3C78]  flex flex-row justify-end ">
+                <Logo img={logoWhite} style="mx-[32px] my-[12px]"/>
             </div>
-            <div>
-
+            <div className="bg-[#EC2026] w-screen h-[10px]">
+               
             </div>
         </div>
     )
--- frontend/src/components/organism/Header/Header.jsx
+++ frontend/src/components/organism/Header/Header.jsx
@@ -3,7 +3,7 @@ import logoWhite from "../../../assets/image 5 1.png"
 
 export function Header(){
     return(
-        <div className="bg-[#1C3C78]">
+        <div className="bg-[#1C3C78] shadow-xl">
             <div className="flex flex-row justify-between " >
                 <p className="text-[#fff] text-[16px] underline underline-offset-8 decoration-red-700 mx-[32px] mt-[24px]">SISTEMA DE EDITAIS</p>
                 <Logo img={logoWhite} style="mx-[32px]"/>

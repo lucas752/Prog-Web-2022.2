55fb3db3089ac840614df3bdf7168402fb57f43d
Nome <miranhaomi@gmail.com>
2023-04-02 20:01:48 -0300

    Atualizando arquivos
    Atualizando a interface do GeneralUser e o AuthController, o erro era por conta que o findByCPF estava com CPF, desrepeitando a nomenclatura do GeneralUser que estava Cpf

--- backend/editais-upe/src/main/java/com/upe/editais/editaisupe/repositories/IGeneralUserRepository.java
+++ backend/editais-upe/src/main/java/com/upe/editais/editaisupe/repositories/IGeneralUserRepository.java
@@ -1,14 +1,13 @@
 package com.upe.editais.editaisupe.repositories;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import com.upe.editais.editaisupe.models.GeneralUser;
 
 public interface IGeneralUserRepository extends CrudRepository<GeneralUser, Long> {
 
-    GeneralUser findByEmail (@Param("email") String email);
+    public GeneralUser findByEmail(String email);
 
-    GeneralUser findByCPF(@Param("cpf") String cpf);
+    public GeneralUser findByCpf(String cpf);
 
 }

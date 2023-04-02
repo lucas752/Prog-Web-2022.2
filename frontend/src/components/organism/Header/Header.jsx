f1ab0ffa9a0313571657cf3a046c8d08ae571a60
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-04-02 13:03:04 -0300

    add: adicionaando componente logo e titulo ao header

--- frontend/src/components/organism/Header/Header.jsx
+++ frontend/src/components/organism/Header/Header.jsx
@@ -5,7 +5,8 @@ export function Header(){
     return(
         <div className="bg-[#1C3C78] shadow-xl">
             <div className="flex flex-row justify-between w-screen" >
-                
+                <p className="text-[#fff] text-[16px] underline underline-offset-8 decoration-red-700 mx-[32px] mt-[24px]">SISTEMA DE EDITAIS</p>
+                <Logo img={logoWhite} style="mx-[32px]"/>
             </div>
         </div>
     )

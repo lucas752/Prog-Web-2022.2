9b68efd5cba5a79d80fff5a41064eba26167d425
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-04-04 14:30:37 -0300

    feat: finalizando componente de adicionar editais

---
+++ frontend/src/components/atomic/LongInput/LongInput.jsx
@@ -0,0 +1,7 @@
+export function LongInput({type, place, func}){
+    return(
+        <div>
+            <textarea  className="resize-none rounded-[12px] p-[5px] text-[#000] w-[22.5rem]" cols="30" rows="3" type={type} placeholder={place} onChange={func}/>
+        </div>
+    )
+}
--- frontend/src/components/template/AddNoticesArea/AddNoticesArea.jsx
+++ frontend/src/components/template/AddNoticesArea/AddNoticesArea.jsx
@@ -1,9 +1,10 @@
 import { Button } from "../../atomic/Button/Button";
 import { Input } from "../../atomic/Input/Input";
+import { LongInput } from '../../atomic/LongInput/LongInput'
 
 export function AddNoticesArea() {
     return(
-        <div className="bg-[#f0f0f0] flex flex-col rounded-xl w-[30rem] items-center justify-center my-10 p-9">
+        <div className="bg-[#f0f0f0] flex flex-col rounded-xl w-[26rem] items-center justify-center my-10 py-10 px-10 max-w-max">
             <div>
                 <div className="mb-[20px]">
                     <p className="my-[4px] text-sm">Título</p>
@@ -11,19 +12,19 @@ export function AddNoticesArea() {
                 </div>
                 <div className="mb-[20px]">
                     <p className="my-[4px] text-sm">Prazo</p>
-                    <Input place={'Insira o prazo'}/>
+                    <Input type={'date'}/>
                 </div>
                 <div className="mb-[20px]">
                     <p className="my-[4px] text-sm" >Descrição</p>
-                    <Input place={'Insira a descrição'}/>
+                    <LongInput place={"Insira a descrição"}/>
                 </div>
                 <div className="mb-[20px]">
                     <p className="my-[4px] text-sm">Requisitos</p>
-                    <Input place={'Insira os requisitos'}/>
+                    <LongInput place={"Insira os requisitos"}/>
                 </div>
                 <div className="mb-[20px]">
                     <p className="my-[4px] text-sm">Critérios de Avaliação</p>
-                    <Input place={'Insira os critérios para avaliação'}/>
+                    <LongInput place={'Insira os critérios para avaliação'}/>
                 </div>
             </div>
 

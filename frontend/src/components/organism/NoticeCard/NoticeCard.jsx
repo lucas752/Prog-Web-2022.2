0a42d5e7c504c2b8565c7007fde3fda6ca29a1eb
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-04-05 16:22:15 -0300

    add: commit de segurança

--- frontend/src/components/organism/NoticeCard/NoticeCard.jsx
+++ frontend/src/components/organism/NoticeCard/NoticeCard.jsx
@@ -1,6 +1,6 @@
 export function NoticeCard({term, domain, requirements, title, description, criteria}) {
     return(
-        <div className="col-span-12 tablet:col-span-12  desktop:col-span-6 bg-[#F0F0F0] rounded-xl min-w-[24rem] w-[24rem] min-h-[27rem] px-4 py-6 shadow-xl mr-4 mb-2.5">
+        <div className="col-span-12 tablet:col-span-12  desktop:col-span-12  bg-[#F0F0F0] rounded-xl min-w-[24rem] w-[24rem] min-h-[27rem] px-4 py-6 shadow-xl mr-4 mb-2.5">
             <h2 className="font-medium text-2xl flex justify-center mb-2">{title}</h2>
             <p className="font-medium text-base mb-2">{description}.</p>
             <div className="text-base mb-2">

d902ca2ac1ca93f9da972175578f53b03ad77f73
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-04-08 15:16:29 -0300

    style: adicionando animação para logo na tela login

--- frontend/src/components/template/LoginArea/LoginArea.jsx
+++ frontend/src/components/template/LoginArea/LoginArea.jsx
@@ -38,7 +38,7 @@ export function LoginArea(){
         <div className="flex justify-center items-center my-[20px] ">
                 <div className="flex flex-col items-center justify-center bg-[#1C3C78] max-w-max p-[40px]  rounded-[12px] text-[#fff] shadow-2xl shadow-indigo-500/40">
 
-                    <div className="max-w-max">
+                    <div className="max-w-max rounded-[12px] hover:shadow-2xl">
                     <Logo img={logoWhite} />
                     </div>
 

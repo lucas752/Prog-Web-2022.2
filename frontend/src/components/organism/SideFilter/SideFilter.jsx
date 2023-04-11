80a77df3f12e9214afdcafb32e7a0a9026cdf0de
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-04-11 01:42:53 -0300

    add: criando base do componente sideFIlter

--- frontend/src/components/organism/SideFilter/SideFilter.jsx
+++ frontend/src/components/organism/SideFilter/SideFilter.jsx
@@ -1,8 +1,15 @@
+import { Input } from "../../atomic/Input/Input";
 export function SideFilter(){
     return(
         <div className="col-span-12 tablet:col-span-12  desktop:col-span-12">
             <p>Áreas</p>
-            <input type="radio" />
+            <div>
+                <div className="flex flex row">
+                    <Input type="radio" name="filter" value="all"/> 
+                    <p className="ml-[5px]">Todos</p>
+                </div>
+                
+            </div>
         </div>
     )
 }

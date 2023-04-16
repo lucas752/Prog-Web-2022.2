5b6c69e9f5b5510f45a29fa77ebc2e611aa43fbe
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-04-16 11:05:28 -0300

    feat: login e cadastro funcional

--- backend/editais-upe/src/main/java/com/upe/editais/editaisupe/controllers/GeneralUserController.java
+++ backend/editais-upe/src/main/java/com/upe/editais/editaisupe/controllers/GeneralUserController.java
@@ -3,6 +3,7 @@ package com.upe.editais.editaisupe.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.upe.editais.editaisupe.repositories.IGeneralUserRepository;
 
 @RestController
 @RequestMapping(path = "/users")
+@CrossOrigin(origins = "*")
 public class GeneralUserController {
 	@Autowired
 	private IGeneralUserRepository uRepository;
--- backend/editais-upe/src/main/java/com/upe/editais/editaisupe/controllers/NoticesController.java
+++ backend/editais-upe/src/main/java/com/upe/editais/editaisupe/controllers/NoticesController.java
@@ -3,6 +3,7 @@ package com.upe.editais.editaisupe.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.upe.editais.editaisupe.repositories.INoticesRepository;
 
 @RestController
 @RequestMapping("/notices")
+@CrossOrigin(origins = "*")
 public class NoticesController {
 	
 	@Autowired
